b0de7cba256301c029fdf712835891102b38c575
Rahul12344 <Therollingreus@gmail.com>
2017-07-25 15:15:48 -0700

    Add files via upload

---
+++ HttpClient.java
@@ -0,0 +1,59 @@
+package httpclient.gui;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.client.methods.HttpUriRequest;
+
+public class HttpClient {
+
+	// http://localhost:8080/RESTfulExample/json/product/post
+	public void login(String name, String password, String email) {
+
+		try {
+
+			DefaultHttpClient httpClient = new DefaultHttpClient();
+			HttpPost postRequest = new HttpPost(
+					"http://localhost:3000/login");
+
+			StringEntity input = new StringEntity(
+					"{\"" + name + "\":\"iPad 4\",\"" + password + "\":\"iPad 4\",\"" + email + "\":\"iPad 4\"}");
+			input.setContentType("application/json");
+			postRequest.setEntity(input);
+
+			HttpResponse response = httpClient.execute(postRequest);
+
+			if (response.getStatusLine().getStatusCode() != 201) {
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ response.getStatusLine().getStatusCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					(response.getEntity().getContent())));
+
+			String output;
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+
+				System.out.println(output);
+			}
+
+			httpClient.getConnectionManager().shutdown();
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
+
+}
